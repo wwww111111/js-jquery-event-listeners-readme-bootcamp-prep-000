ac35ccab4404000594a4a16ce335fba5eff4d1c7
patient-computer-3602 <thelionking898@gmail.com>
2020-03-21 16:24:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
+
+//define functions here
+
+function getIt() {
+  $('p').on('click', function(event) {
+     alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(event) {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $('form').on('keydown', function(event) {
+    if (event.which == 71) {
+      alert('Pressed the G key');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(event) {
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+
